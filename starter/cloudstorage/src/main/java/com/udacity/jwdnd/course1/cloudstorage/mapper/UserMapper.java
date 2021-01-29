58aae1d32456ea5d8195d5d6d34190b19f1b4b39
titanium25 <idpm7N95tLPLskg>
2021-01-29 10:00:22 +0200

    New user registration fix

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/UserMapper.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/UserMapper.java
@@ -10,7 +10,7 @@ import org.apache.ibatis.annotations.Select;
 @Mapper
 public interface UserMapper {
     @Select("SELECT * FROM USERS WHERE userName = #{userName}")
-    User getUser(String username);
+    User getUser(String userName);
 
     @Insert("INSERT INTO USERS (userName, salt, password, firstName, lastName) VALUES(#{userName}, #{salt}, #{password}, #{firstName}, #{lastName})")
     @Options(useGeneratedKeys = true, keyProperty = "userId")
