071c1f682028fd8abe0517bfc385ccfff63a8abb
titanium25 <idpm7N95tLPLskg>
2021-02-03 11:07:30 +0200

    new error page

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/CredentialController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/CredentialController.java
@@ -6,23 +6,24 @@ import com.udacity.jwdnd.course1.cloudstorage.services.CredentialService;
 import com.udacity.jwdnd.course1.cloudstorage.services.UserService;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
-@RequestMapping("/credential")
 public class CredentialController {
 
     private UserService userService;
     private CredentialService credentialService;
-
 
     public CredentialController(UserService userService, CredentialService credentialService) {
         this.userService = userService;
         this.credentialService = credentialService;
     }
 
-    @PostMapping
+    @PostMapping("/credential")
     public String addCredential(Authentication authentication,
                                 @ModelAttribute Credential credential,
                                 RedirectAttributes redirectAttributes){
@@ -35,21 +36,19 @@ public class CredentialController {
         } else {
             credentialService.updateCredential(credential);
         }
-
         redirectAttributes.addFlashAttribute("activeTab", "credentials");
         redirectAttributes.addFlashAttribute("isSuccess", true);
 
         return "redirect:/result";
     }
 
-    @GetMapping("/delete/{credentialId}")
+    @GetMapping("/credential/delete/{credentialId}")
     public String deleteCredential(RedirectAttributes redirectAttributes,
+                                   @ModelAttribute Credential credential,
                                    @PathVariable Integer credentialId) {
-        Credential credential = credentialService.getByCredentialId(credentialId);
-        redirectAttributes.addFlashAttribute("isSuccess", true);
-        redirectAttributes.addFlashAttribute("errorText", "URL " + credential.getUrl() + " was deleted");
-        redirectAttributes.addFlashAttribute("activeTab", "credentials");
         credentialService.deleteCredential(credentialId);
+        redirectAttributes.addFlashAttribute("activeTab", "credentials");
+        redirectAttributes.addFlashAttribute("isSuccess", true);
         return "redirect:/result";
     }
 }
--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/CredentialService.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/CredentialService.java
@@ -4,23 +4,18 @@ import com.udacity.jwdnd.course1.cloudstorage.mapper.CredentialMapper;
 import com.udacity.jwdnd.course1.cloudstorage.models.Credential;
 import org.springframework.stereotype.Service;
 
-import java.security.SecureRandom;
-import java.util.Base64;
 import java.util.List;
 
 @Service
 public class CredentialService {
 
     private CredentialMapper credentialMapper;
-    private EncryptionService encryptionService;
 
-    public CredentialService(CredentialMapper credentialMapper, EncryptionService encryptionService) {
+    public CredentialService(CredentialMapper credentialMapper) {
         this.credentialMapper = credentialMapper;
-        this.encryptionService = encryptionService;
     }
 
     public int addCredential(Credential credential){
-        encryptCredential(credential);
         return credentialMapper.insert(credential);
     }
 
@@ -28,26 +23,11 @@ public class CredentialService {
         return credentialMapper.getAllByUserId(userId);
     }
 
-    public Credential getByCredentialId(Integer credentialId) {
-        return credentialMapper.getByCredentialId(credentialId);
-    }
-
     public void deleteCredential(Integer credentialId) {
         credentialMapper.deleteByCredentialId(credentialId);
     }
 
     public int updateCredential(Credential credential) {
-        encryptCredential(credential);
         return credentialMapper.updateCredential(credential);
-    }
-
-    private void encryptCredential(Credential credential) {
-        SecureRandom random = new SecureRandom();
-        byte[] key = new byte[16];
-        random.nextBytes(key);
-        String encodedKey = Base64.getEncoder().encodeToString(key);
-        String encryptedPassword = encryptionService.encryptValue(credential.getPassword(), encodedKey);
-        credential.setKey(encodedKey);
-        credential.setPassword(encryptedPassword);
     }
 }
