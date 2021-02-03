cb2ec3d64d93ec84dc04115d32a3f8eaa82288fc
titanium25 <idpm7N95tLPLskg>
2021-02-03 12:54:38 +0200

    New login page

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/LoginController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/LoginController.java
@@ -2,9 +2,7 @@ package com.udacity.jwdnd.course1.cloudstorage.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/login")
