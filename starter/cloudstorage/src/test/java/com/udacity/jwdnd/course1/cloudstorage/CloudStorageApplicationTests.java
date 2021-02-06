16a487adcb12322f92109554853dba2a3ab32dc5
titanium25 <idpm7N95tLPLskg>
2021-02-06 13:10:56 +0200

    add signup and login test

--- starter/cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplicationTests.java
+++ starter/cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplicationTests.java
@@ -1,41 +1,62 @@
 package com.udacity.jwdnd.course1.cloudstorage;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 
+import java.util.concurrent.TimeUnit;
+
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CloudStorageApplicationTests {
 
-	@LocalServerPort
-	private int port;
+    public static WebDriver driver;
+    public String baseURL;
+    @LocalServerPort
+    private int port;
 
-	private WebDriver driver;
+    @BeforeAll
+    static void beforeAll() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+    }
 
-	@BeforeAll
-	static void beforeAll() {
-		WebDriverManager.chromedriver().setup();
-	}
+    @AfterAll
+    public static void afterAll() {
+        driver.quit();
+        driver = null;
+    }
 
-	@BeforeEach
-	public void beforeEach() {
-		this.driver = new ChromeDriver();
-	}
+    @BeforeEach
+    public void beforeEach() {
+        baseURL = baseURL = "http://localhost:" + port;
+    }
 
-	@AfterEach
-	public void afterEach() {
-		if (this.driver != null) {
-			driver.quit();
-		}
-	}
+    @Test
+    public void testUserSignup() throws InterruptedException {
+        String firstName = "Zori";
+        String lastName = "Dahan";
+        String username = "User_1";
+        String password = "123";
+        String password2 = "123";
+        String email = "123";
 
-	@Test
-	public void getLoginPage() {
-		driver.get("http://localhost:" + this.port + "/login");
-		Assertions.assertEquals("Login", driver.getTitle());
-	}
+        driver.get(baseURL + "/signup");
+
+        SignupPage signupPage = new SignupPage(driver);
+        signupPage.signup(firstName, lastName, username, password, password2, email);
+
+        driver.get(baseURL + "/login");
+
+        LoginPage loginPage = new LoginPage(driver);
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        loginPage.login(username, password);
+    }
 
 }
---
+++ starter/cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/SignupPage.java
@@ -0,0 +1,45 @@
+package com.udacity.jwdnd.course1.cloudstorage;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class SignupPage {
+
+    @FindBy(id = "inputUserName")
+    private WebElement usernameField;
+
+    @FindBy(id = "inputPassword")
+    private WebElement passwordField;
+
+    @FindBy(id = "inputPassword2")
+    private WebElement password2Field;
+
+    @FindBy(id = "inputFirstName")
+    private WebElement firstNameField;
+
+    @FindBy(id = "inputLastName")
+    private WebElement lastNameField;
+
+    @FindBy(id = "inputEmail")
+    private WebElement emailField;
+
+    @FindBy(id = "submit-button")
+    private WebElement submitButton;
+
+    public SignupPage(WebDriver webDriver) {
+        PageFactory.initElements(webDriver, this);
+    }
+
+    public void signup(String firstName, String lastName, String userName, String password, String password2, String email) {
+        this.firstNameField.sendKeys(firstName);
+        this.lastNameField.sendKeys(lastName);
+        this.usernameField.sendKeys(userName);
+        this.passwordField.sendKeys(password);
+        this.password2Field.sendKeys(password2);
+        this.emailField.sendKeys(email);
+        this.submitButton.click();
+    }
+
+}
